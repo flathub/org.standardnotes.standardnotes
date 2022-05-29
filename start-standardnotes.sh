aa77206612ba75979a75cc9e53bbe9bc6ab1e290
proletarius101 <54175165+proletarius101@users.noreply.github.com>
2022-05-29 19:58:27 +0800

    add support for wayland by default (#89)
    * add support for wayland by default
    
    * fix typo
    
    * build: update to ref add7f724b65c9bc2749806e88f4d47a40c6c02cd
    
    * fix incorrect tagging
    
    * fix persisting backups
    
    * build: update to ref v3.20.0
    
    * fix build
    
    * fix yarn update url template
    
    * build: update to ref v3.20.0
    
    * enable wayland by default if available

---
+++ start-standardnotes.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -oue pipefail
+
+export FLATPAK_ID="${FLATPAK_ID:-org.standardnotes.standardnotes}"
+export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+
+# Wayland support can be optionally disabled like so:
+# flatpak override --user --env=USE_WAYLAND=0 org.standardnotes.standardnotes
+declare -i USE_WAYLAND="${USE_WAYLAND:-1}"
+declare -i EXIT_CODE=0
+
+if [[ "${USE_WAYLAND}" -eq 1 && "${XDG_SESSION_TYPE}" == "wayland" ]]; then
+    zypak-wrapper /app/standardnotes/standard-notes --enable-features=UseOzonePlatform --ozone-platform=wayland $@ || EXIT_CODE=$?
+    # Fall back to x11 if failed to launch under Wayland. Otherwise, exit normally
+    [[ "${EXIT_CODE}" -ne 133 ]] && exit "${EXIT_CODE}"
+fi
+
+zypak-wrapper /app/standardnotes/standard-notes $@
