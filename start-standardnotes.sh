6033c9f7ba3a4b621d8f3b5bd33e1f0e547a1c38
proletarius101 <proletarius101@protonmail.com>
2022-07-23 21:20:47 +0800

    disable wayland by default

--- start-standardnotes.sh
+++ start-standardnotes.sh
@@ -7,7 +7,7 @@ export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
 # Wayland support can be optionally enabled like so:
 # flatpak override --user --env=USE_WAYLAND=1 org.standardnotes.standardnotes
-declare -i USE_WAYLAND="${USE_WAYLAND:-1}"
+declare -i USE_WAYLAND="${USE_WAYLAND:-0}"
 declare -i EXIT_CODE=0
 
 if [[ "${USE_WAYLAND}" -eq 1 && "${XDG_SESSION_TYPE}" == "wayland" ]]; then
