bb9de0e1b5605849ca5beb0e5601d62d6eabbc75
proletarius101 <54175165+proletarius101@users.noreply.github.com>
2023-01-04 22:41:41 +0800

    enable wayland by default (#251)
    * enable wayland by default
    
    since it has been updated to Electron 21 (https://github.com/standardnotes/app/commit/a896b4d9cfcda1221aad1d847eec104efd8757ba), the wayland support has been completed. We should enable it by default.
    
    * fix socket flag not changed to wayland

--- start-standardnotes.sh
+++ start-standardnotes.sh
@@ -7,11 +7,11 @@ export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
 # Wayland support can be optionally enabled like so:
 # flatpak override --user --env=USE_WAYLAND=1 org.standardnotes.standardnotes
-declare -i USE_WAYLAND="${USE_WAYLAND:-0}"
+declare -i USE_WAYLAND="${USE_WAYLAND:-1}"
 declare -i EXIT_CODE=0
 
 if [[ "${USE_WAYLAND}" -eq 1 && "${XDG_SESSION_TYPE}" == "wayland" ]]; then
-    zypak-wrapper /app/standardnotes/standard-notes --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland $@ || EXIT_CODE=$?
+    zypak-wrapper /app/standardnotes/standard-notes --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations $@ || EXIT_CODE=$?
     # Fall back to x11 if failed to launch under Wayland. Otherwise, exit normally
     [[ "${EXIT_CODE}" -ne 133 ]] && exit "${EXIT_CODE}"
 fi
