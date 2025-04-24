eb706a93396f965abf031c723578507cd869b51e
proletarius101 <54175165+proletarius101@users.noreply.github.com>
2025-04-23 22:57:32 -0700

    enable IME support on wayland

--- start-standardnotes.sh
+++ start-standardnotes.sh
@@ -5,4 +5,4 @@ set -oue pipefail
 export FLATPAK_ID="${FLATPAK_ID:-org.standardnotes.standardnotes}"
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
-zypak-wrapper /app/standardnotes/standard-notes $@
+zypak-wrapper /app/standardnotes/standard-notes --gtk-version=4 --enable-wayland-ime $@
